b3446b60cc3cb6191483292f1f0763db1470c059
YasmineM311 <95468771+YasmineM311@users.noreply.github.com>
2023-03-13 10:52:24 +0100

    Added new file to combine multiple patient data files

---
+++ combining_patient_data.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from health_data_functions import *
+
+## make sure you are pointing to the correct directory
+
+# creating a list of all files in the directory
+file_list = os.listdir()
+
+# creating a list of names to be used as dictionary keys 
+# each corresponding to one of the files in the directory
+filenames = []
+for i in range(1,len(file_list)+1):
+    name = "file_" + str(i)
+    filenames.append(name)
+
+# creating a dictionary to store the dataframe corresponding to each file 
+dataframe_dict = {}
+
+for name in filenames:
+    for file in file_list:
+        dataframe_dict[name] = fetch_data_from_file(file)
+
+for i in dataframe_dict.values():
+    print(i.head())
--- data_to_csv.py
+++ data_to_csv.py
@@ -2,8 +2,13 @@
 import pandas as pd
 from health_data_functions import *
 
+pd.options.display.max_rows = 2000
 
-df = fetch_data_from_file('C:/Users/y_mus/OneDrive - Hochschule Luzern/HSLU/Thesis/Code/my apple watch data/HealthAutoExport-2023-01-25.json')
+df = fetch_data_from_file('C:/Users/y_mus/OneDrive - Hochschule Luzern/HSLU/Thesis/Code/my apple watch data/HealthAutoExport-2023-01-22.json')
 df.to_csv('test.csv')
+
+# print(df[df.index > "12:00"])
+
+
 
 
